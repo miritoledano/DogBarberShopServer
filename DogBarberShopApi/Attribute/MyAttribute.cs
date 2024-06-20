8584adde34d198a6338d59be6b1719cdfcbed333
miritoledano <mt0548553246@gmail.com>
2024-06-20 23:32:47 +0300

    Initial commit

---
+++ DogBarberShopApi/Attribute/MyAttribute.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc;
+using DogBarberShopBl.intarfaces;
+using System.Collections;
+using DogBarberShopBl.servers;
+// Assuming your business logic services are in this namespace
+
+namespace DogBarberShopApi.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class MyAttribute : ActionFilterAttribute
+    {
+        public override async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            var serviceProvider = context.HttpContext.RequestServices;
+            var queueBl = serviceProvider.GetService<IqueueBl>();
+            var homeBl = serviceProvider.GetService<IhomeBl>();
+
+            if (context.ActionArguments.TryGetValue("id", out object idObj) && idObj is int id)
+            {
+                var queueUserId = queueBl.GetUserById(id);
+                var userId = homeBl.GetUserId();
+                if (queueUserId == userId)
+                {
+                    await next(); // Continue with the action
+                }
+                else
+                {
+                    context.Result = new ForbidResult(); // Return 403 Forbidden if user is not authorized
+                }
+            }
+            else
+            {
+                context.Result = new BadRequestObjectResult("Invalid request"); // Return 400 Bad Request if id is missing or not an integer
+            }
+        }
+
+    }
+}
---
+++ DogBarberShopApi/Controllers/HomeController.cs
@@ -0,0 +1,82 @@
+﻿using DogBarberShopApi.Attributes;
+using DogBarberShopBl.intarfaces;
+using DogBarberShopBl.servers;
+using DogBarberShopEntitis;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace DogBarberShopApi.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class HomeController : ControllerBase
+    {
+
+        private readonly ILogger<HomeController> _logger;
+        private readonly IMemoryCache _memoryCache;
+        private readonly IhomeBl _homeBL;
+        public HomeController(ILogger<HomeController> logger, IMemoryCache memoryCache, IhomeBl homeBL)
+        {
+            _logger = logger;
+            _memoryCache = memoryCache;
+            _homeBL = homeBL;
+        }
+        [HttpPost]
+        public IActionResult GetHeader()
+        {
+            try
+            {
+                var header = _homeBL.GetHeader();
+                return Ok($"דף הבית - {header}");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Error on GetHeader, Message: {ex.Message}," +
+                    $" InnerException: {ex.InnerException}, StackTrace: {ex.StackTrace}");
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+       
+
+        [HttpGet]
+        public IActionResult GetUserName()
+        {
+            try
+            {
+                string userName = _homeBL.GetUserName();
+                return Ok(userName);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Error on GetUserName, Message: {ex.Message}," +
+                                       $" InnerException: {ex.InnerException}, StackTrace: {ex.StackTrace}");
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+        [HttpGet]
+        public IActionResult GetUserId()
+        {
+            try
+            {
+                int userId = _homeBL.GetUserId();
+                return Ok(userId);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Error on GetUserName, Message: {ex.Message}," +
+                                       $" InnerException: {ex.InnerException}, StackTrace: {ex.StackTrace}");
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+    }
+}
+        
+
+      
+   
+
+
